30e24b43e4d4a2a39231750a845054349e3ac85c
TostersLV <rpeizums@gmail.com>
2024-12-12 14:03:45 +0200

    commit 3

--- Database.php
+++ Database.php
@@ -1,19 +1,26 @@
 <?php
 
 class Database {
+
+    public $pdo;
     // uztaisit metodi query
-    public function query($sql){
-        //Data source name
-        $dsn = "mysql:host=localhost;port=3306;user=root;password=;dbname=blog;charset=utf8mb4";
+    public function __construct($config) {
+        
+        $dsn = "mysql:" . http_build_query($config, "", ";");
 
         // PHP data object
-        $pdo = new PDO($dsn);
-
+        $this->pdo = new PDO($dsn);
+        $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    }
+ 
+    public function query($sql){
+        //Data source name
+       
         // sagatavot vaicajumu
-        $statement = $pdo->prepare($sql);
+        $statement = $this->pdo->prepare($sql);
         // izpildit vaicajumu
         $statement->execute();
-        return $statement->fetchAll(PDO::FETCH_ASSOC);
+        return $statement;
     }
 }
 
--- index.php
+++ index.php
@@ -2,20 +2,19 @@
 
 require "functions.php";
 require "Database.php";
+$config = require("config.php"); 
 
-$db = new Database();
-$posts = $db->query("SELECT * FROM posts");
+$db = new Database($config["database"]);
 
 
 
+$posts = $db->query("SELECT * FROM posts")->fetchAll();
 
 echo "hi<br>";
-
 
 echo "<ul>";
 foreach($posts as $post){
     echo "<li>" . $post["content"] . "</li>";
-
 
 };
 echo "</ul>";
